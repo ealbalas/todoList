65e234eaf15628eb97208c3c2ccdd8433d6dcc54
Emad Albalas <ealbalas@csu.fullerton.edu>
2023-04-06 22:08:15 -0700

    add ejs file. create control flow in ejs file

--- app.js
+++ app.js
@@ -2,12 +2,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express();
+const app = express()
+app.set('view engine', 'ejs')
 
 app.get("/", function(req, res){
-  res.send("Hello");
+  var today = new Date()
+  var currentDay = today.getDay()
+  var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  var day = week[currentDay]
+
+//   if (currentDay == 0 || currentDay == 6) {
+//     day = "Weekend"
+//   } else {
+//     day = "Weekday"
+// }
+    res.render('list', {kindOfDay: day})
 });
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
-});
+})
