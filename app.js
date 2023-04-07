4328bf03f79f01a618aad50214c0ad4db78e30f4
Emad Albalas <ealbalas@csu.fullerton.edu>
2023-04-06 23:19:14 -0700

    Add better date format. Add logic that adds items to list

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+let ejs = require("ejs")
 
 const app = express()
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+
+var items = []
 
 app.get("/", function(req, res){
   var today = new Date()
-  var currentDay = today.getDay()
-  var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  var day = week[currentDay]
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+
+  var day = today.toLocaleTimeString("en-US", options)
 
 //   if (currentDay == 0 || currentDay == 6) {
 //     day = "Weekend"
 //   } else {
 //     day = "Weekday"
 // }
-    res.render('list', {kindOfDay: day})
-});
+    res.render('list', {kindOfDay: day, todoItems: items})
+})
+
+app.post("/", function(req, res){
+    var item = req.body.todo
+    items.push(item)
+    res.redirect("/")
+})
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
